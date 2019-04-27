b3e12486f3997aea21d15174c0f23f0376298933
bro-wer <bronek.werla@gmail.com>
2019-04-27 20:21:03 +0200

    Added args validation

--- main.py
+++ main.py
@@ -4,16 +4,37 @@ def ValidateArgsCount():
     if len(sys.argv) != 4:
         raise Exception("Unexpected args count!")
 
+def ValidateArgs():
+    try:
+        CheckIfInt(sys.argv[1])
+        CheckIfInt(sys.argv[2])
+        CheckIfValidEquation(sys.argv[3])
+    except Exception as e:
+        raise e
+
+def CheckIfInt(var):
+    try:
+        temp = int(var)
+    except Exception as e:
+        raise Exception("Provided variables are not int types!")
+
+def CheckIfValidEquation(var):
+    validEquationTypes = ["+", "-", "*" ,"/"]
+    if not var in validEquationTypes:
+        raise Exception("Provided equation type is not valid!")
+
 def CalculateEquation():
-    firstArg = sys.argv[1]
-    secondArg = sys.argv[2]
+    firstArg = int(sys.argv[1])
+    secondArg = int(sys.argv[2])
     equation = sys.argv[3]
-    print(firstArg + secondArg)
+    result = eval("{}{}{}".format(firstArg, "+", secondArg))
+    print(result)
 
 
 if __name__ == '__main__':
     try:
         ValidateArgsCount()
+        ValidateArgs()
         CalculateEquation()
     except Exception as e:
         print(e)
